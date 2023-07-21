359e7f468de376049bce54f11dfde73378799900
Gautam Magar <puskarmagar687@gmail.com>
2023-07-21 12:39:52 +0545

    feat: Adding increse , reset , decrese function

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -1,23 +1,46 @@
 import React from 'react'
 import {Card , Button} from 'react-bootstrap'
+import { useState } from 'react'
 
 const counter = () => {
 
+  const [ number ,setnumber] =useState (50)
+
+  const Increment =(e)=>{
+    e.preventDefault();
+    setnumber(number + 1)
+  }
+
+  const Decrement =(e)=>{
+    e.preventDefault();
+
+    setnumber(number -1)
+
+  }
+
+  const reset =(e)=>{
+    e.preventDefault();
+
+    setnumber(0)
+
+  }
+
   return (
+
 
     <>
 
       {
 
         <Card>
-          <Card.Title>0</Card.Title>
+          <Card.Title>{number}</Card.Title>
           <Card.Body></Card.Body>
 
           <Card.Footer>
 
-            <Button className='btn m-3'>Increment +</Button>
-            <Button className='btn btn-danger m-3'>Reset</Button>
-            <Button className='btn btn-secondary m-3'>Decrement -</Button>
+            <Button className='btn m-3'onClick={Increment}>Increment +</Button>
+            <Button className='btn btn-danger m-3' onClick={reset}>Reset </Button>
+            <Button className='btn btn-secondary m-3' onClick={Decrement} >Decrement -</Button>
           </Card.Footer>
 
         </Card>
